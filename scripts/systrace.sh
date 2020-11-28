13c9fd15fd3f06266e511535f81c26d2fd8c4ca3
Cheng Jian <gatieme@163.com>
2020-11-28 22:48:40 +0800

    script: add systrace.sh

---
+++ scripts/systrace.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TRACING_PATH=/sys/kernel/debug/tracing
+
+trace_clean()
+{
+	echo 0 > $TRACING_PATH/tracing_on
+	echo 0 > $TRACING_PATH/events/enable
+	echo > $TRACING_PATH/trace
+}
+
+
+get_systrace()
+{
+	local sleepTime=$1
+
+	rm -rf trace.html
+	echo > $TRACING_PATH/trace
+
+	echo 1 > $TRACING_PATH/events/sched/sched_wakeup/enable
+	echo 1 > $TRACING_PATH/events/sched/sched_wakeup_new/enable
+	echo 1 > $TRACING_PATH/events/sched/sched_switch/enable
+	echo 1 > $TRACING_PATH/events/irq/enable
+
+	echo 1 > $TRACING_PATH/tracing_on
+
+	sleep $sleepTime
+
+	echo 0 > $TRACING_PATH/tracing_on
+	echo 0 > $TRACING_PATH/events/enable
+
+	cat $TRACING_PATH/trace > trace.html
+	echo > $TRACING_PATH/trace
+}
+
+
+trap "trace_clean" 1 2 3 9 15
+
+trace_clean
+get_systrace $1
+trace_clean
+
